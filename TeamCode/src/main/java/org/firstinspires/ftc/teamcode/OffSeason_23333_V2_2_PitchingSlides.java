73864eaf90f8a589fdf21d8468c1e9122c14a53e
Crashandburn001 <lwhit503@eq.edu.au>
2025-06-05 16:54:14 +1000

    Re-added drivebase code (BAD AI BAD), fixed some set pos stuff. :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OffSeason_23333_V2_2_PitchingSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OffSeason_23333_V2_2_PitchingSlides.java
@@ -17,23 +17,41 @@ public class OffSeason_23333_V2_2_PitchingSlides extends LinearOpMode {
     public static float adjustmentsClawRoll = 0.5f;
     public static int adjustmentsSlidePitch = 20;
 
-    // Variables for Collect Set Pos
     public static int COLLECT_SLIDE_PITCH = 0;
-    public static int COLLECT_SLIDE_RETRACTION = -1900;
+    public static int COLLECT_SLIDE_RETRACTION = -600;
     public static double COLLECT_WRIST_PITCH = 0.2;
 
-    //Variables for SCORE Set Pos
     public static int SCORE_SLIDE_PITCH = 300;
-    public static int SCORE_SLIDE_RETRACTION = -600;
+    public static int SCORE_SLIDE_RETRACTION = -1200;
     public static double SCORE_WRIST_PITCH = 0.8;
 
-    //Variables for IDLE Set Pos
     public static int IDLE_SLIDE_PITCH = 0;
     public static int IDLE_SLIDE_RETRACTION = 0;
     public static double IDLE_WRIST_PITCH = 0.5;
 
+    public static int SPECIMEN_COLLECT_SLIDE_PITCH = 150;
+    public static int SPECIMEN_COLLECT_SLIDE_RETRACTION = -700;
+    public static double SPECIMEN_COLLECT_WRIST_PITCH = 0.3;
+
+    public static int SPECIMEN_SCORE_SLIDE_PITCH = 450;
+    public static int SPECIMEN_SCORE_SLIDE_RETRACTION = -1300;
+    public static double SPECIMEN_SCORE_WRIST_PITCH = 0.85;
+
+    // ArmState without enum
+    public static final int STATE_IDLE = 0;
+    public static final int STATE_MOVING_TO_COLLECT = 1;
+    public static final int STATE_MOVING_TO_SCORE = 2;
+    public static final int STATE_MOVING_TO_IDLE = 3;
+    public static final int STATE_MOVING_TO_SPECIMEN_COLLECT = 4;
+    public static final int STATE_MOVING_TO_SPECIMEN_SCORE = 5;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+
         DcMotor slidePitch = hardwareMap.dcMotor.get("slidePitch");
         DcMotor slideRetraction = hardwareMap.dcMotor.get("slideRetraction");
 
@@ -41,6 +59,14 @@ public class OffSeason_23333_V2_2_PitchingSlides extends LinearOpMode {
         CRServo clawGrip = hardwareMap.crservo.get("clawGrip");
 
         Servo wristPitch = hardwareMap.servo.get("wristPitch");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         slidePitch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slidePitch.setDirection(DcMotor.Direction.REVERSE);
@@ -52,32 +78,45 @@ public class OffSeason_23333_V2_2_PitchingSlides extends LinearOpMode {
 
         slideRetraction.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideRetraction.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         waitForStart();
 
         targetSlidePitchPosition = slidePitch.getCurrentPosition();
         targetSlideRetractionPosition = slideRetraction.getCurrentPosition();
 
-        while (opModeIsActive()) {
+        int armState = STATE_IDLE;
+        boolean commandIssued = false;
+        final int SLIDE_PITCH_TOLERANCE = 10;
 
-            // Manual Slide Pitch control
-            if (gamepad1.y) {
-                targetSlidePitchPosition += adjustmentsSlidePitch;
-            } else if (gamepad1.x) {
-                targetSlidePitchPosition -= adjustmentsSlidePitch;
-            }
+        while (opModeIsActive()) {
+            // Driving
+            double drive = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(turn), 1);
+            double fl = (drive + strafe + turn) / denominator;
+            double fr = (drive - strafe - turn) / denominator;
+            double bl = (drive - strafe + turn) / denominator;
+            double br = (drive + strafe - turn) / denominator;
+
+            frontLeft.setPower(fl);
+            frontRight.setPower(fr);
+            backLeft.setPower(bl);
+            backRight.setPower(br);
+
+            // Manual slide pitch control
+            if (gamepad1.y) targetSlidePitchPosition += adjustmentsSlidePitch;
+            else if (gamepad1.x) targetSlidePitchPosition -= adjustmentsSlidePitch;
 
             slidePitch.setTargetPosition(targetSlidePitchPosition);
             slidePitch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             slidePitch.setPower(1);
 
-            // Manual Slide Retraction
-            if ((gamepad1.right_trigger > 0.1)) {
-                targetSlideRetractionPosition += 10;
-            } else if ((gamepad1.left_trigger > 0.1)) {
-                targetSlideRetractionPosition -= 10;
-            }
+            // Manual slide retraction control
+            if (gamepad1.right_trigger > 0.1) targetSlideRetractionPosition += 10;
+            else if (gamepad1.left_trigger > 0.1) targetSlideRetractionPosition -= 10;
 
-            // Clamp retraction
             targetSlideRetractionPosition = Math.min(targetSlideRetractionPosition, 40);
             targetSlideRetractionPosition = Math.max(targetSlideRetractionPosition, -1920);
 
@@ -85,25 +124,19 @@ public class OffSeason_23333_V2_2_PitchingSlides extends LinearOpMode {
             slideRetraction.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             slideRetraction.setPower(1);
 
-            // Manual wrist pitch (gamepad1)
-            if (gamepad1.dpad_up) {
-                wristPitchPos += adjustmentsWristPitch;
-            } else if (gamepad1.dpad_down) {
-                wristPitchPos -= adjustmentsWristPitch;
-            }
+            // Wrist pitch control
+            if (gamepad1.dpad_up) wristPitchPos += adjustmentsWristPitch;
+            else if (gamepad1.dpad_down) wristPitchPos -= adjustmentsWristPitch;
+
             wristPitchPos = Math.max(0, Math.min(1, wristPitchPos));
             wristPitch.setPosition(wristPitchPos);
 
-            // Claw roll (CRServo - gamepad1)
-            if (gamepad1.dpad_left) {
-                clawRoll.setPower(adjustmentsClawRoll);
-            } else if (gamepad1.dpad_right) {
-                clawRoll.setPower(-adjustmentsClawRoll);
-            } else {
-                clawRoll.setPower(0);
-            }
+            // Claw roll (CR servo)
+            if (gamepad1.dpad_left) clawRoll.setPower(adjustmentsClawRoll);
+            else if (gamepad1.dpad_right) clawRoll.setPower(-adjustmentsClawRoll);
+            else clawRoll.setPower(0);
 
-            // Claw grip (CRServo - gamepad1)
+            // Claw grip (CR servo)
             if (gamepad1.right_bumper) {
                 clawGrip.setPower(0.8);
                 gamepad1.rumble(1);
@@ -114,26 +147,89 @@ public class OffSeason_23333_V2_2_PitchingSlides extends LinearOpMode {
                 gamepad1.stopRumble();
             }
 
-            // Set positions (gamepad2)
-            if (gamepad2.a) { // COLLECT
+            // Arm states
+            if (gamepad2.a && !commandIssued) {
                 targetSlidePitchPosition = COLLECT_SLIDE_PITCH;
-                targetSlideRetractionPosition = COLLECT_SLIDE_RETRACTION;
-                wristPitchPos = COLLECT_WRIST_PITCH;
-            } else if (gamepad2.y) { // SCORE
+                armState = STATE_MOVING_TO_COLLECT;
+                commandIssued = true;
+            } else if (gamepad2.y && !commandIssued) {
                 targetSlidePitchPosition = SCORE_SLIDE_PITCH;
-                targetSlideRetractionPosition = SCORE_SLIDE_RETRACTION;
-                wristPitchPos = SCORE_WRIST_PITCH;
-            } else if (gamepad2.b) { // IDLE
+                armState = STATE_MOVING_TO_SCORE;
+                commandIssued = true;
+            } else if (gamepad2.b && !commandIssued) {
                 targetSlidePitchPosition = IDLE_SLIDE_PITCH;
-                targetSlideRetractionPosition = IDLE_SLIDE_RETRACTION;
-                wristPitchPos = IDLE_WRIST_PITCH;
+                armState = STATE_MOVING_TO_IDLE;
+                commandIssued = true;
+            } else if (gamepad2.x && !commandIssued) {
+                targetSlidePitchPosition = SPECIMEN_COLLECT_SLIDE_PITCH;
+                armState = STATE_MOVING_TO_SPECIMEN_COLLECT;
+                commandIssued = true;
+            } else if (gamepad2.dpad_up && !commandIssued) {
+                targetSlidePitchPosition = SPECIMEN_SCORE_SLIDE_PITCH;
+                armState = STATE_MOVING_TO_SPECIMEN_SCORE;
+                commandIssued = true;
             }
 
+            if (!gamepad2.a && !gamepad2.y && !gamepad2.b && !gamepad2.x && !gamepad2.dpad_up)
+                commandIssued = false;
+
+            boolean slidePitchInPosition = Math.abs(slidePitch.getCurrentPosition() - targetSlidePitchPosition) < SLIDE_PITCH_TOLERANCE;
+
+            switch (armState) {
+                case STATE_MOVING_TO_COLLECT:
+                    if (slidePitchInPosition) {
+                        targetSlideRetractionPosition = COLLECT_SLIDE_RETRACTION;
+                        wristPitchPos = COLLECT_WRIST_PITCH;
+                        armState = STATE_IDLE;
+                    }
+                    break;
+                case STATE_MOVING_TO_SCORE:
+                    if (slidePitchInPosition) {
+                        targetSlideRetractionPosition = SCORE_SLIDE_RETRACTION;
+                        wristPitchPos = SCORE_WRIST_PITCH;
+                        armState = STATE_IDLE;
+                    }
+                    break;
+                case STATE_MOVING_TO_IDLE:
+                    if (slidePitchInPosition) {
+                        targetSlideRetractionPosition = IDLE_SLIDE_RETRACTION;
+                        wristPitchPos = IDLE_WRIST_PITCH;
+                        armState = STATE_IDLE;
+                    }
+                    break;
+                case STATE_MOVING_TO_SPECIMEN_COLLECT:
+                    if (slidePitchInPosition) {
+                        targetSlideRetractionPosition = SPECIMEN_COLLECT_SLIDE_RETRACTION;
+                        wristPitchPos = SPECIMEN_COLLECT_WRIST_PITCH;
+                        armState = STATE_IDLE;
+                    }
+                    break;
+                case STATE_MOVING_TO_SPECIMEN_SCORE:
+                    if (slidePitchInPosition) {
+                        targetSlideRetractionPosition = SPECIMEN_SCORE_SLIDE_RETRACTION;
+                        wristPitchPos = SPECIMEN_SCORE_WRIST_PITCH;
+                        armState = STATE_IDLE;
+                    }
+                    break;
+            }
+
+            // Final wrist update
+            wristPitch.setPosition(wristPitchPos);
+
+            // Telemetry
+            telemetry.addData("Drive FL", fl);
+            telemetry.addData("Drive FR", fr);
+            telemetry.addData("Drive BL", bl);
+            telemetry.addData("Drive BR", br);
+
+            telemetry.addData("ArmState", armState);
             telemetry.addData("SlidePitch Pos", slidePitch.getCurrentPosition());
             telemetry.addData("SlideRetraction Pos", slideRetraction.getCurrentPosition());
             telemetry.addData("Target Pitch", targetSlidePitchPosition);
             telemetry.addData("Target Retract", targetSlideRetractionPosition);
             telemetry.addData("Wrist Pitch", wristPitchPos);
+            telemetry.addData("Slide Pitch Power", slidePitch.getPower());
+            telemetry.addData("Slide Retraction Power", slideRetraction.getPower());
             telemetry.update();
         }
     }
